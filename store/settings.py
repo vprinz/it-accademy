9ca8a6dd87e2a18eb3499d5e862437a898e3194c
Vokler <valerypavlikov@yandex.ru>
2022-03-02 10:34:34 +0300

    added displaying errors for login.html

--- store/settings.py
+++ store/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
