e6c9e1b3b1ea2514b62a8b3841aa8a8aedd28232
Vokler <valerypavlikov@yandex.ru>
2022-03-02 10:21:46 +0300

    added logic for login()

--- users/views.py
+++ users/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from users.forms import UserLoginForm
 
 
 def login(request):
-    context = {'title': 'Store - Авторизация'}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'title': 'Store - Авторизация', 'form': form}
     return render(request, 'users/login.html', context)
 
 
