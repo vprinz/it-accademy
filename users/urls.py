d603e0eb896b2e8d82ed34c7af31c32d84584d63
Vokler <valerypavlikov@yandex.ru>
2022-03-02 12:37:47 +0300

    added profile.html

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from users.views import login, registration, logout
+from users.views import login, registration, logout, profile
 
 app_name = 'users'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login, name='login'),  # ../users/login/
     path('registration/', registration, name='registration'),  # ../users/registration/
     path('logout/', logout, name='logout'),  # ../users/logout/
+    path('profile/', profile, name='profile'),  # ../users/profile/
 ]
--- users/views.py
+++ users/views.py
@@ -35,6 +35,10 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
