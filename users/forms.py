943e277bd0cc8bd8260745b7f4bcf7ec1b18b740
Vokler <valerypavlikov@yandex.ru>
2022-03-02 10:30:58 +0300

    added custom style for UserLoginForm

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+
     class Meta:
         model = User
         fields = ('username', 'password')
