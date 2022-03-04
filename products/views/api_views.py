94b827ecf5b1e7de37a170c4bb11dd060ecbb5b5
Vokler <valerypavlikov@yandex.ru>
2022-03-04 10:00:18 +0300

    added permissions

--- products/views/api_views.py
+++ products/views/api_views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
 
 from products.serializers import ProductCategorySerializer, ProductSerializer
 from products.models import ProductCategory, Product
@@ -7,6 +9,8 @@ from products.models import ProductCategory, Product
 class ProductCategoryViewSet(ModelViewSet):
     queryset = ProductCategory.objects.all()
     serializer_class = ProductCategorySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
 
 
 class ProductViewSet(ModelViewSet):
--- store/settings.py
+++ store/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    
+    'rest_framework.authtoken',
+
     'products',
     'users',
 ]
